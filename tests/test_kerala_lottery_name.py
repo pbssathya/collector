7fe0100f96718e1d2922cd8d0ed3fef4c1b9a6f8
Sathya P B <p.sathya.b@gmail.com>
2026-08-23 00:19:01 +0530

    Cover scheduled and held Kerala heading layout

--- tests/test_kerala_lottery_name.py
+++ tests/test_kerala_lottery_name.py
@@ -19,6 +19,17 @@ in KARUNYA LOTTERY NO.KR-636th DRAW held on:- 13/01/2024,2:00 PM
     assert Parser()._extract_lottery_name(text) == "KARUNYA LOTTERY NO.KR-636th DRAW"
 
 
+def test_extracts_heading_without_caring_about_scheduled_on_suffix():
+    lines = [
+        "KARUNYA PLUS LOTTERY NO.KN-610th DRAW scheduled on 12/02/2026 at 3:00 PM,",
+        "and held on:- 13/02/2026,1:30 PM",
+    ]
+
+    parser = Parser()
+    assert parser._extract_lottery_name(lines) == "KARUNYA PLUS LOTTERY NO.KN-610th DRAW"
+    assert parser._extract_draw_date(lines) == "13/02/2026"
+
+
 def test_extracts_2023_bumper_heading_with_year_from_real_pdf_shape():
     text = """
 SUMMER BUMPER 2023 LOTTERY NO.BR-90th DRAW held on:- 19/03/2023,2:00 PM
