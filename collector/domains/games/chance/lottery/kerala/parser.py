265e17dfc3a7b42f26e24d30d77d050f47a67002
Sathya P B <p.sathya.b@gmail.com>
2026-08-22 23:59:30 +0530

    Fix scheduled-on Kerala lottery heading lookahead

--- collector/domains/games/chance/lottery/kerala/parser.py
+++ collector/domains/games/chance/lottery/kerala/parser.py
@@ -18,7 +18,7 @@ class Parser:
         r"(?im)^[ \t]*(?:in[ \t]+)?"
         r"([A-Z][A-Z0-9 \t-]*?LOTTERY(?:[ \t]+LOTTERY)?[ \t]+"
         r"NO\.[A-Z]+-\d+(?:st|nd|rd|th)[ \t]+DRAW)"
-        r"(?=[ \t]+(?:held|scheduled)[ \t]+on(?::|-)|[ \t]*$)"
+        r"(?=[ \t]+(?:held[ \t]+on:-|scheduled[ \t]+on[ \t]+)|[ \t]*$)"
     )
 
     PRIZE_HEADING_RE = re.compile(
