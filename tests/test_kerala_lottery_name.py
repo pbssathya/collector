846178ae01b853204c1cc1f578783348100c049c
Sathya P B <p.sathya.b@gmail.com>
2026-08-22 23:56:20 +0530

    Add regression test for scheduled-on Kerala draw heading

--- tests/test_kerala_lottery_name.py
+++ tests/test_kerala_lottery_name.py
@@ -19,6 +19,15 @@ in KARUNYA LOTTERY NO.KR-636th DRAW held on:- 13/01/2024,2:00 PM
     assert Parser()._extract_lottery_name(text) == "KARUNYA LOTTERY NO.KR-636th DRAW"
 
 
+def test_extracts_heading_when_draw_is_followed_by_scheduled_on_text():
+    text = """
+KARUNYA PLUS LOTTERY NO.KN-610th DRAW scheduled on 12/02/2026 at 3:00 PM,
+and held on:- 13/02/2026,1:30 PM
+"""
+
+    assert Parser()._extract_lottery_name(text) == "KARUNYA PLUS LOTTERY NO.KN-610th DRAW"
+
+
 def test_extracts_2023_bumper_heading_with_year_from_real_pdf_shape():
     text = """
 SUMMER BUMPER 2023 LOTTERY NO.BR-90th DRAW held on:- 19/03/2023,2:00 PM
